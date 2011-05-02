0d6925eb50446af9d2445d8b70dfb6eabd309f08
Rickard Lund <natural_blindfold@hotmail.com>
2011-05-02 12:53:11 +0200

    Visual modifications to index page

--- mupalon/index.php
+++ mupalon/index.php
@@ -8,7 +8,7 @@
 	<script src="jquery-ui.js" type="text/javascript" charset="utf-8"></script>
 	<script src="mupalon.js" type="text/javascript" charset="utf-8"></script>
 	<link rel="stylesheet" href="mupalon.css" type="text/css" media="screen" charset="utf-8" />
-	<title>index</title>
+	<title><?php echo ucwords(preg_replace('/(\.php|\/)/', ' | ', $_SERVER['PHP_SELF'])); ?></title>
 	
 </head>
 
@@ -17,13 +17,7 @@
 	<div id="player_area">
 	<?php require('get_songs.php'); ?>
 	</div>
-	<form action="file_submit.php" enctype="multipart/form-data" method="post" accept-charset="utf-8" id="mus_sub">
-		<input type="hidden" name="action" value="upload" id="action" />
-		<p>
-			<input type="file" name="song" value="" id="song" />
-			<input type="submit" name="submit" value="Upload" />
-		</p>
-	</form>
+	<a href="upload.php"> Upload more files </a>
 </div>
 </body>
 </html>
